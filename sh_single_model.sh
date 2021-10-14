c6fc1c74166bbf2da264bfc59915840da9ab94b3
NamCyan <nkimxte@gmail.com>
2021-10-14 14:04:29 +0000

    update

--- sh_single_model.sh
+++ sh_single_model.sh
@@ -7,12 +7,12 @@ python3 ./run_single_model.py \
     --learning_rate 2e-5 \
     --weight_decay 1e-3 \
     --num_train_epochs 10 \
-    --max_seq_length 512 \
-    --doc_stride 64 \
-    --max_query_length=32 \
-    --per_gpu_train_batch_size=2 \
-    --per_gpu_eval_batch_size=2 \
-    --gradient_accumulation_steps 8 \
+    --max_seq_length 400 \
+    --doc_stride 128 \
+    --max_query_length=64 \
+    --per_gpu_train_batch_size=4 \
+    --per_gpu_eval_batch_size=4 \
+    --gradient_accumulation_steps 4 \
     --warmup_steps=128 \
     --output_dir result/av_xlm_roberta_lr2e-5_len256_bs16_ep2_wm814 \
     --do_lower_case \
@@ -21,4 +21,4 @@ python3 ./run_single_model.py \
     --save_steps 2000 \
     --logging_steps 2500 \
     --n_best_size 20 \
-    --max_answer_length=40
+    --max_answer_length=200
