b3a27b4a19f28eb94d9f7b0e38c0cafb25d24de6
NamCyan <nkimxte@gmail.com>
2021-10-14 14:09:02 +0000

    update

--- sh_single_model.sh
+++ sh_single_model.sh
@@ -1,6 +1,7 @@
 python3 ./run_single_model.py \
     --model_type xlm_roberta_mixlayer_large \
     --do_eval \
+    --do_train \
     --version_2_with_negative \
     --train_file ../data/VLSP_data/VLSP_train_split.json \
     --predict_file ../data/VLSP_data/VLSP_dev_split.json \
